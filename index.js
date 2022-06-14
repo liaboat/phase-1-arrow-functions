2f7f5d2831a7d4f64afcbc4cc7a302914817fc83
Lydia <lydia.boateng@student.moringaschool.com>
2022-06-14 08:49:14 +0100

    lab work completed

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(){
+    return 2000/100;
+}
+const square =(parameter1 ) => parameter1 * parameter1
+
+const add = (parameter1, parameter2) => {
+    let add = parameter1 + parameter2;
+    return add;
+}
